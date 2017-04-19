840ada08ebf68806d884a9dbe1be48b64b394dd9
Bryan Ngo <bngo@ucdavis.edu>
2017-04-18 17:45:35 -0700

    Added method to grab API key from local machine and updated documentation

--- src/main.java
+++ src/main.java
@@ -6,6 +6,9 @@ import com.robrua.orianna.type.core.match.ParticipantStats;
 import com.robrua.orianna.type.core.matchlist.MatchReference;
 import com.robrua.orianna.type.core.summoner.Summoner;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -15,8 +18,9 @@ class Example {
 
 
         // Sensitive information
-        final String APIKEY = "";
-        String summonerName = "";
+        final String APIKEY = getBryanAPIKey("League");
+        ;
+        String summonerName = "Lunoski";
 
         // Set the environment
         RiotAPI.setRegion(Region.NA);
@@ -41,8 +45,6 @@ class Example {
 
     }
 
-
-
     private static boolean getMatchOutcome(Match match, int participantID) {
 
         int teamID;
@@ -52,7 +54,7 @@ class Example {
         else teamID = 1;
 
         return match.getTeams().get(teamID).getWinner();
-    }
+    } // returns if the participant actually won the match
 
     private static String getSummonerMatchStats(Match match, Summoner summoner) {
 
@@ -79,10 +81,12 @@ class Example {
         summonerStats = participants.get(participantID).getStats();
 
 
+        // get the KDA
         kills = summonerStats.getKills();
-        assists = summonerStats.getAssists();
         deaths = summonerStats.getDeaths();
+        assists = summonerStats.getAssists();
 
+        // construct the match summary string
         matchSummary = "Champ: " + summonerChamp + ". Got " +
                 kills + " kills, " +
                 assists + " assists, " +
@@ -92,6 +96,7 @@ class Example {
         else
             matchSummary += " . Defeat :(";
 
+        // return the match summary;
         return matchSummary;
     }
 
@@ -108,4 +113,42 @@ class Example {
         return cal.get(Calendar.YEAR);
 
     } // method to get the year from a match
-}
+
+    private static String getBryanAPIKey(String APIcompany) {
+
+        // holds the list of api keys
+        List<String> records = new ArrayList<String>();
+
+        // open the file and add the keys to the list
+        try
+        {
+            BufferedReader reader = new BufferedReader(new FileReader("/Users/Bryan1/keys.text"));
+            String line;
+            while ((line = reader.readLine()) != null)
+            {
+                records.add(line);
+            }
+            reader.close();
+        }
+
+        // error
+        catch (Exception e)
+        {
+            System.err.format("Exception occurred trying to read '%s'.", "/Users/Bryan1/keys.text");
+            e.printStackTrace();
+            return null;
+        }
+
+        // return the requested api key
+        for (String record : records)
+            if (record.contains(APIcompany)) {
+                String[] tokens = record.split("=");
+                return tokens[1];
+            }
+
+
+        return "ERROR";
+
+    }
+
+} // returns an APIKey.
